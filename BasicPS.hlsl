75ef4283c198f3a03bc9c00317642cef47bf0463
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-06-15 16:13:17 +0900

    完了

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -1,4 +1,10 @@
+cbuffer ConstBufferMaterial : register(b0)
+{
+	// êFÅiRGBAÅj
+	float4 color;
+}
+
 float4 main() : SV_TARGET
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+	return color;
 }
--- main.cpp
+++ main.cpp
@@ -248,6 +248,56 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 #pragma endregion
 
+	/////////////////////
+	// --描画初期化処理-- //
+	/////////////////////
+
+#pragma region
+
+	// --定数バッファに送るデータをまとめた型-- //
+	struct ConstBufferDataMaterial
+	{
+		// 色（RGBA）
+		XMFLOAT4 color;
+	};
+
+	// --ヒープ設定-- //
+	D3D12_HEAP_PROPERTIES cbHeapProp{};
+	cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD;// -> GPUへの転送用
+
+	// --リソース設定-- //
+	D3D12_RESOURCE_DESC cbResourceDesc{};
+	cbResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	cbResourceDesc.Width = (sizeof(ConstBufferDataMaterial) + 0xff) & ~0xff;// -> 256バイトアラインメント
+	cbResourceDesc.Height = 1;
+	cbResourceDesc.DepthOrArraySize = 1;
+	cbResourceDesc.MipLevels = 1;
+	cbResourceDesc.SampleDesc.Count = 1;
+	cbResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	// --定数バッファの生成-- //
+	ID3D12Resource * constBuffMaterial = nullptr;
+	result = device->CreateCommittedResource(
+		&cbHeapProp,// -> ヒープ設定
+		D3D12_HEAP_FLAG_NONE,
+		&cbResourceDesc,// -> リソース設定
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&constBuffMaterial)
+	);
+
+	// --定数バッファのマッピング-- //
+	ConstBufferDataMaterial * constMapMaterial = nullptr;
+	
+	// --マッピング-- //
+	result = constBuffMaterial->Map(0, nullptr, (void**)&constMapMaterial);
+	assert(SUCCEEDED(result));
+
+	// 値を書き込むと自動的に転送される
+	constMapMaterial->color = XMFLOAT4(1, 0, 0, 0.5f);// -> RGBAで半透明の赤
+
+#pragma endregion
+
 	// --ゲームループ-- //
 	while (true)
 	{
@@ -507,12 +557,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		pipelineDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB; // 0~255指定のRGBA
 		pipelineDesc.SampleDesc.Count = 1; // 1ピクセルにつき1回サンプリング
 
+		// --ルートパラメータの設定-- //
+		D3D12_ROOT_PARAMETER rootParam = {};
+		rootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;// --> 定数のバッファビュー
+		rootParam.Descriptor.ShaderRegister = 0;// ------------------> 定数バッファ番号
+		rootParam.Descriptor.RegisterSpace = 0;// -------------------> デフォルト値
+		rootParam.ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;// -> 全てのシェーダーから見える
+
 		// --ルートシグネチャ-- //
 		ID3D12RootSignature * rootSignature;
 		
 		// --ルートシグネチャの設定-- //
 		D3D12_ROOT_SIGNATURE_DESC rootSignatureDesc{};
 		rootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+		rootSignatureDesc.pParameters = &rootParam;// -> ルートパラメータの先頭アドレス
+		rootSignatureDesc.NumParameters = 1;// --------> ルートパラメータ数
 		
 		// --ルートシグネチャのシリアライズ-- //
 		ID3DBlob * rootSigBlob = nullptr;
@@ -563,6 +622,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 		// --頂点バッファビューの設定コマンド-- //
 		commandList->IASetVertexBuffers(0, 1, &vbView);
+
+		// --定数バッファビュー（CBV）の設定コマンド-- //
+		commandList->SetGraphicsRootConstantBufferView(0, constBuffMaterial->GetGPUVirtualAddress());
 
 		// --描画コマンド-- //
 		commandList->DrawInstanced(_countof(vertices), 1, 0, 0); // 全ての頂点を使って描画
