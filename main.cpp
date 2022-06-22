e12b0b595c4341d692ae96a7753d88c18cd53e70
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-06-22 17:41:28 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -348,11 +348,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	// --単位行列を代入-- //
 	constMapTransform->mat = XMMatrixIdentity();
 
-	constMapTransform->mat.r[0].m128_f32[0] = 2.0f / window_width;
-	constMapTransform->mat.r[1].m128_f32[1] = -2.0f / window_height;
+	// --平行投影行列の計算-- //
+	constMapTransform->mat = XMMatrixOrthographicOffCenterLH(
+		2.0f / window_width, -1.0f,
+		1.0f, -2.0f / window_height,
+		0.0f, 1.0f
+	);
 
-	constMapTransform->mat.r[3].m128_f32[0] = -1.0f;
-	constMapTransform->mat.r[3].m128_f32[1] = 1.0f;
+	// --透視投影行列の計算-- //
+	XMMATRIX matProjection = XMMatrixPerspectiveFovLH(
+		XMConvertToRadians(45.0f),// -----------> 上下画角45度
+		(float)window_width / window_height,// -> アスペクト比（画面横幅/画面縦幅）
+		0.1f, 1000.0f// ------------------------> 前端、奥端
+	);
+
+	// --次回、ここでビュー変換行列を計算
+
+	// --定数バッファに転送-- //
+	constMapTransform->mat = matProjection;
 
 	//// --横方向ピクセル数-- //
 	//const size_t textureWidth = 256;
@@ -495,10 +508,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	// --頂点データ-- //
 	Vertex vertices[] = {
-		{{   0.0f, 100.0f, 0.0f}, {0.0f, 1.0f}},// -> 左下
-		{{   0.0f,   0.0f, 0.0f}, {0.0f, 0.0f}},// -> 左上
-		{{ 100.0f, 100.0f, 0.0f}, {1.0f, 1.0f}},// -> 右下
-		{{ 100.0f,   0.0f, 0.0f}, {1.0f, 0.0f}},// -> 右上
+		{{ -50.0f, -50.0f, 50.0f}, {0.0f, 1.0f}},// -> 左下
+		{{ -50.0f,  50.0f, 50.0f}, {0.0f, 0.0f}},// -> 左上
+		{{  50.0f, -50.0f, 50.0f}, {1.0f, 1.0f}},// -> 右下
+		{{  50.0f,  50.0f, 50.0f}, {1.0f, 0.0f}},// -> 右上
 	};
 
 	uint16_t indices[] = {
